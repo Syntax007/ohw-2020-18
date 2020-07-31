af0c032bd99366ac919cbca92d187970d9bd6006
Lord-Chris <maduekechris65@gmail.com>
2020-07-31 03:40:12 +0100

    Created home page and game mode page

---
+++ lib/ui/views/gamemode_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:truthordare/ui/shared/main_button.dart';
+
+class GameMode extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/background.jpg'), fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 150.sp,
+              ),
+              Text(
+                'Game Mode',
+                style: TextStyle(fontSize: 70.sp, fontWeight: FontWeight.bold),
+              ),
+              Spacer(flex: 2,),
+              MainButton(
+                color: Colors.yellow,
+                text: 'Kids',
+                onPressed: () {
+                  // TODO: Implement kids button
+                },
+              ),
+              Spacer(),
+              MainButton(
+                color: Colors.deepOrange,
+                text: 'Adults',
+                onPressed: () {
+                  // TODO: Implement Adults button
+                },
+              ),
+              Spacer(flex: 4,)
+            ],
+          )),
+    );
+  }
+}
